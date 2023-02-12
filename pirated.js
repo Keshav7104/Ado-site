5a5a71e23bd672a75e0ed83bdaf4f397d24f6fa0
Keshav <keshavbhutaani1360@gmail.com>
2023-02-12 11:41:46 +0530

    made some changes in nav bar and solved video issues

--- pirated.js
+++ pirated.js
@@ -1,26 +1,17 @@
 var i=0;
 var j=0;
-var txt= 'a game in which people try to find a hidden prize by following special signs or clues which have been left in different places but it is much more than that,this hunt for treasure or in other words to be elgible to become pirate is fueled by greed,dedication,problem-solving,teamworkand much more so are you to ready to give this test to be a Adovedi "Pirate".';
-var txt2 ='in this clash between pirates to get treasure,ofcouse not gold,and more than that to get some fun, some knowledge,some memories you have to wonder through our lush green campus by solving all clues one by one to ultimately acquire the treasure but what a minute what is the treasure,so no one knows it yet untill they found it.';
+var txt= 'a game in which people try to find a hidden prize by following special signs or clues which have been left in different places but it is much more than that,this hunt for treasure or in other words to be elgible to become pirate is fueled by greed, dedication, problem-solving, teamworkand much more so are you to ready to give this test to be a Adovedi "Pirate". in this clash between pirates to get treasure,ofcouse not gold,and more than that to get some fun, some knowledge,some memories you have to wonder through our lush green campus by solving all clues one by one to ultimately acquire the treasure but what a minute what is the treasure,so no one knows it yet untill they found it.';
 var speed=30;
 function typing() {
     var ele2 =document.getElementById("type");
     ele2.classList.add("bcl");
-    if((i+j)<(txt.length+txt2.length)){
-        if(i<txt.length){
+    if(i<txt.length){
             document.getElementById("type").innerHTML+=txt.charAt(i);
             i++;
             setTimeout(typing,speed);
-        }
-        else{
-            document.getElementById("type").innerHTML+=txt2.charAt(j);
-            j++;
-            setTimeout(typing,speed);
-        }
     }
  
 }
-
 function diss(){
     var ele = document.getElementById("dis");
     ele.classList.add("dis");
